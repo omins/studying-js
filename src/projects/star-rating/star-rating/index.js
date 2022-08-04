9b259dbe3a504f17b8af47315fd5961ac29301d6
Kevin Minseok Oh <oms3749@gmail.com>
2022-08-05 01:12:24 +0900

    feat: 정적인 별점 컴포넌트 구현

--- src/projects/star-rating/star-rating/index.js
+++ src/projects/star-rating/star-rating/index.js
@@ -1,1 +1,15 @@
-// do something!
+const StarRating = $container => {
+  const { maxRating } = $container.dataset;
+  const starRatingContainer = document.createElement('div');
+  starRatingContainer.className = 'star-rating-container';
+
+  for (let i = 0; i < maxRating; i++) {
+    const star = document.createElement('i');
+    star.className = 'bx bxs-star';
+    starRatingContainer.appendChild(star);
+  }
+
+  $container.append(starRatingContainer);
+};
+
+export default StarRating;
