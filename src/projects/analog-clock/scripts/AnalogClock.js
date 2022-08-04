a2ffcdf927f88df0f355a1a0592f871317bd42cb
Kevin Minseok Oh <oms3749@gmail.com>
2022-08-04 18:16:12 +0900

    feat: 아날로그 시계 동작 구현

--- src/projects/analog-clock/scripts/AnalogClock.js
+++ src/projects/analog-clock/scripts/AnalogClock.js
@@ -1,5 +1,36 @@
 const AnalogClock = $container => {
-  // do something!
+  $container.innerHTML = `<div class="hand hour"></div>
+  <div class="hand minute"></div>
+  <div class="hand second"></div>
+  <div class="time time1">|</div>
+  <div class="time time2">|</div>
+  <div class="time time3">|</div>
+  <div class="time time4">|</div>
+  <div class="time time5">|</div>
+  <div class="time time6">|</div>
+  <div class="time time7">|</div>
+  <div class="time time8">|</div>
+  <div class="time time9">|</div>
+  <div class="time time10">|</div>
+  <div class="time time11">|</div>
+  <div class="time time12">|</div>`;
+
+  const secondsMinuteDeg = 6;
+  const handHour = document.querySelectorAll('.hand.hour');
+  const handMinute = document.querySelectorAll('.hand.minute');
+  const handSecond = document.querySelectorAll('.hand.second');
+
+  setInterval(() => {
+    const currentDate = new Date();
+    const seconds = currentDate.getSeconds();
+    const minutes = currentDate.getMinutes();
+    const hours = currentDate.getHours();
+    const hourDeg = (hours > 12 ? (hours - 12) * 30 : hours * 30) + minutes * 0.5;
+
+    handHour.forEach(e => e.style.setProperty('--deg', hourDeg));
+    handMinute.forEach(e => e.style.setProperty('--deg', secondsMinuteDeg * minutes));
+    handSecond.forEach(e => e.style.setProperty('--deg', secondsMinuteDeg * seconds));
+  }, 1000);
 };
 
 export default AnalogClock;
