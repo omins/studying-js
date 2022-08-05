6d727d60c0c6b868846aa4f10f7afa5ed2ca5c3c
Kevin Minseok Oh <oms3749@gmail.com>
2022-08-05 18:11:32 +0900

    feat: 별점 컴포넌트 mouseover, mouseout 핸들링

--- src/projects/star-rating/app.js
+++ src/projects/star-rating/app.js
@@ -10,7 +10,6 @@ starRatingStyle.setAttribute("href", "star-rating/theme.css")
 document.head.insertBefore(starRatingStyle, scriptEl)
 
 $containers.forEach(($container, i) => {
-  // star-rating 컨테이너 요소의 참조를 StarRating 함수에 전달해 star 요소들로 구성된 star-rating 요소를 동적 생성한다.
   StarRating($container);
 
   // 이벤트 'rating-change'를 캐치해 화면에 표시한다.
--- src/projects/star-rating/star-rating/index.js
+++ src/projects/star-rating/star-rating/index.js
@@ -1,3 +1,53 @@
+// 별점이 저장되어있지 않을 때
+// 별점이 저장되어 있을 때
+
+const starOnEnter = el => {
+  // TODO selected인지 아닌지 판별
+  el.classList.add('hovered');
+};
+
+const starOnLeave = el => {
+  // TODO selected인지 아닌지 판별
+  el.classList.remove('hovered');
+};
+
+const starOnMouseOver = e => {
+  const idx = e.target.dataset.starIdx;
+  if (!idx) return;
+
+  const container = e.target.closest('.star-rating-container');
+
+  if (container.hasChildNodes()) {
+    const childrens = container.childNodes;
+    for (let i = 0; i <= idx; i++) {
+      starOnEnter(childrens[i]);
+    }
+  }
+};
+
+const starOnMouseout = e => {
+  const targetFrom = e.target;
+  const targetTo = e.relatedTarget;
+
+  if (!targetFrom.classList.contains('bxs-star')) return;
+
+  const starRatingContainer = targetFrom.closest('.star-rating-container');
+
+  if (!targetTo.classList.contains('bxs-star')) {
+    // 별점 컴포넌트에서 mouseout
+    if (starRatingContainer.hasChildNodes()) {
+      const childrens = starRatingContainer.childNodes;
+
+      for (let i = 0; i < childrens.length; i++) {
+        starOnLeave(childrens[i]);
+      }
+    }
+  } else {
+    // 별 하나에서 mouseout
+    starOnLeave(targetFrom);
+  }
+};
+
 const StarRating = $container => {
   const { maxRating } = $container.dataset;
   const starRatingContainer = document.createElement('div');
@@ -5,11 +55,15 @@ const StarRating = $container => {
 
   for (let i = 0; i < maxRating; i++) {
     const star = document.createElement('i');
+    star.dataset.starIdx = i;
     star.className = 'bx bxs-star';
     starRatingContainer.appendChild(star);
   }
 
   $container.append(starRatingContainer);
+
+  starRatingContainer.addEventListener('mouseover', starOnMouseOver);
+  starRatingContainer.addEventListener('mouseout', starOnMouseout);
 };
 
 export default StarRating;
